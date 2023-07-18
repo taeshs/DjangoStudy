f762ed23f136ef77520e67ddc88bba04d399f2c7
taeshs <taejustar@gmail.com>
2023-07-18 15:44:10 +0900

    url 패턴 수정, view 수정

--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,7 @@ def post_list(request):
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     # try:
     #     post = Post.published.get(id=id)
     # except Post.DoesNotExist:
@@ -19,7 +19,12 @@ def post_detail(request, id):
     # return render(request,
     #               'blog/post/detail.html'
     #               , {'post': post})
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
 
 
