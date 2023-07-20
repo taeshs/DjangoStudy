71aabaa2633ead1b722fc9f265acaaaef887a689
taeshs <taejustar@gmail.com>
2023-07-20 10:41:14 +0900

    commentform 생성 ,post_comment 뷰함수 추가 ,url 경로추가

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 
-from blog.forms import EmailPostForm
+from blog.forms import EmailPostForm, CommentForm
 from blog.models import Post
 from django.core.mail import send_mail
 
@@ -66,3 +67,15 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.Post)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post':post,'form':form,'comment':comment})
