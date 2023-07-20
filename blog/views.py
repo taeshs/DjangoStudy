dc428832aab6017e9859bc20cc997e8690b77d7d
taeshs <taejustar@gmail.com>
2023-07-20 13:21:59 +0900

    detail 페이지 댓글 폼 추가

--- blog/views.py
+++ blog/views.py
@@ -53,29 +53,24 @@ def post_list(request):
 
 
 def post_detail(request, year, month, day, post):
-    # try:
-    #     post = Post.published.get(id=id)
-    # except Post.DoesNotExist:
-    #     raise Http404("No Post found.")
-    # return render(request,
-    #               'blog/post/detail.html'
-    #               , {'post': post})
-    post = get_object_or_404(Post,
-                             status=Post.Status.PUBLISHED,
-                             slug=post,
-                             publish__year=year,
-                             publish__month=month,
-                             publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post,
+                             publish__year=year, publish__month=month, publish__day=day)
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, 'blog/post/detail.html',
+                  {'post': post, 'comments': comments, 'form': form})
 
 
 @require_POST
 def post_comment(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
     comment = None
-    form = CommentForm(data=request.Post)
+    form = CommentForm(data=request.POST)
     if form.is_valid():
         comment = form.save(commit=False)
         comment.post = post
         comment.save()
-    return render(request, 'blog/post/comment.html', {'post':post,'form':form,'comment':comment})
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
+
+
+
