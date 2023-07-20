0754f10d83571ea7f2dba2aa652036e7a629dddd
taeshs <taejustar@gmail.com>
2023-07-20 09:47:05 +0900

    모델 admin 사이트 등록

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from blog.models import Post
+from blog.models import Post,Comment
 
 # Register your models here.
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'author', 'publish', 'status']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']
